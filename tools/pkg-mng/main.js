a498caf9ffc40de867b0c1b0e7f4724bd0328e91
Quramy <yosuke.kurami@gmail.com>
2020-10-27 23:09:40 +0900

    chore: fmt

--- tools/pkg-mng/main.js
+++ tools/pkg-mng/main.js
@@ -1,10 +1,9 @@
 const Arborist = require("@npmcli/arborist");
 
 async function main() {
-
   const tree = await new Arborist().loadActual();
   for (const [name, edge] of tree.edgesOut.entries()) {
-    if (edge.type === 'workspace') {
+    if (edge.type === "workspace") {
       console.log(name, edge.to.realpath);
     }
   }
