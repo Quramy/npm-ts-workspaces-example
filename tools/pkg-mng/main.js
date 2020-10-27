c3d75759a40d269124154cc47820b8acdce27f29
Quramy <yosuke.kurami@gmail.com>
2020-10-27 23:07:20 +0900

    chore: workspaces list via arborist

---
+++ tools/pkg-mng/main.js
@@ -0,0 +1,13 @@
+const Arborist = require("@npmcli/arborist");
+
+async function main() {
+
+  const tree = await new Arborist().loadActual();
+  for (const [name, edge] of tree.edgesOut.entries()) {
+    if (edge.type === 'workspace') {
+      console.log(name, edge.to.realpath);
+    }
+  }
+}
+
+main();
